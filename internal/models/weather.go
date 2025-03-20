a4e7965152b97bdfd82bcf68b11605fc2439c669
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-03-20 14:16:16 +0700

    feat: download api

--- internal/models/weather.go
+++ internal/models/weather.go
@@ -7,16 +7,16 @@ import (
 type Weather struct {
 	ID                 uint      `gorm:"primaryKey"` // Auto-incrementing primary key
 	CreatedAt          time.Time // Timestamp of record creation
-	Pressure           float32   `gorm:"not null"`
-	RelativeHumidity   float32   `gorm:"not null"`
-	Temperature        float32   `gorm:"not null"`
-	WindDirection      float32   `gorm:"not null"`
-	WindSpeed          float32   `gorm:"not null"`
-	CHP1               float32   `gorm:"not null"`
-	DirectSun          float32   `gorm:"not null"`
-	GlobalSun          float32   `gorm:"not null"`
-	DiffuseSun         float32   `gorm:"not null"`
-	RainFall           float32   `gorm:"not null"`
-	AllDayIllumination float32   `gorm:"not null"`
-	PM25               float32   `gorm:"not null"`
+	Pressure           float32   `json:"pressure" gorm:"not null"`
+	RelativeHumidity   float32   `json:"relative_humidity" gorm:"not null"`
+	Temperature        float32   `json:"temperature" gorm:"not null"`
+	WindDirection      float32   `json:"wind_direction" gorm:"not null"`
+	WindSpeed          float32   `json:"wind_speed" gorm:"not null"`
+	CHP1               float32   `json:"chp1" gorm:"not null"`
+	DirectSun          float32   `json:"direct_sun" gorm:"not null"`
+	GlobalSun          float32   `json:"global_sun" gorm:"not null"`
+	DiffuseSun         float32   `json:"diffuse_sun" gorm:"not null"`
+	RainFall           float32   `json:"rain_fall" gorm:"not null"`
+	AllDayIllumination float32   `json:"all_day_illumination" gorm:"not null"`
+	PM25               float32   `json:"pm25" gorm:"not null"`
 }
